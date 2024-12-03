d34cd1d12c86b0f85798ba903bf8d5d161164dc9
Hitesh171 <127680967+Hitesh171@users.noreply.github.com>
2024-12-03 13:14:35 +0530

    file uploaded

--- Desc.jsx
+++ Desc.jsx
@@ -1,12 +1,19 @@
 function Desc() {
   return (
-    <p>
-      This particular Box uses&nbsp;
-      <a href="https://github.com/ChainSafe/web3.js" target="_blank" rel="noreferrer">
-        web3.js
-      </a>
-      , a popular Ethereum library.
-    </p>
+    <>
+      <p>
+        Take a look at <span className="code">client/src/contexts/EthContext</span>.
+        This context maintains a global state and provides web3.js functionalities
+        to the rest of the app.
+      </p>
+      <p>
+        Feel free to remove any component or styling that you don't need, and
+        extend <span className="code">EthContext</span> to your dapp's needs.
+      </p>
+      <p>
+        Happy hacking!
+      </p>
+    </>
   );
 }
 
